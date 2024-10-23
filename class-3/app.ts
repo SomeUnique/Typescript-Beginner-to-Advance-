1c3d041230fcdab02a9dec5da8edfa06ddf24d2a
SomeUnique <expertbychance@gmail.com>
2024-10-23 13:33:44 +0500

    Add files via upload

---
+++ class-3/app.ts
@@ -0,0 +1,55 @@
+let favoriteColor = "black";
+console.log(favoriteColor);
+
+favoriteColor = "blue";
+console.log(favoriteColor);
+
+let lastName = "jawwad";
+console.log(lastName);
+
+// declare a variable
+let day = "thursday";
+console.log(day);
+
+//  updating a variable 
+day = "friday";
+console.log(day);
+
+// DATA TYPES
+ console.log(typeof day)
+
+ let age = 20
+//  error
+// age = "twenty"
+ console.log(typeof age)
+
+//  console.log("Type Day ==>",typeof day);
+
+// boolean ( true or false)
+let isPresent = true;
+console.log(typeof isPresent);
+console.log(isPresent);
+// is age is 20
+const number = 20;
+console.log(number == 20);
+ 
+// for use of datatypes
+let Age: string ="20";
+Age = "twenty";
+console.log(Age);
+console.log("Type Day ==>",typeof Age);
+
+//  datatype {any}
+// let Age: any ="20"
+// Age = "twenty"
+
+let agee: any = 20;
+agee = '20'
+agee = true;
+console.log(agee);
+//------------------------
+// const {constant/one time}
+// jo variable change hoskta voh let mn ayega
+
+
+
---
+++ class-4/main.ts
@@ -0,0 +1,36 @@
+let num1: number = 19;
+let num2: number = 15;
+let result = num2 -  num1;
+console.log(result);
+//------------------
+let num3: number = 20;
+console.log(num1 + num2 * num3);
+//--------------------
+let num4:number = 21;
+console.log(num1 + num2 / (num1 * num4))
+//..................
+let apple = 5;
+let  bags = 2;
+let remainder = apple%bags;
+console.log(remainder);
+//.....................
+let a1 = 0
+console.log(a1++);//(post increement)
+console.log(a1);
+console.log(a1);
+console.log(++a1);//(phly add phr result)(pre)
+console.log(a1++);
+//..................
+let aa= 0
+let bb=5
+let cc
+cc=aa++ + aa++ + ++bb + aa++ + aa-- + ++bb
+console.log(cc)
+//.........................
+//OPERATORS....
+//  1.Assignment operators
+//  2.comparision operators
+let number1 = 5;
+let number2 = 2;
+console.log(number1==number2);
+console.log(number1!=number2);
